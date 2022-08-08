daadab42a50dde8f5fc4a4ac7aef783e9c9ce4ec
Aravind Krishna <akaravindk59@gmail.com>
2022-08-08 12:09:43 +0530

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const dotenv = require('dotenv')
+dotenv.config()
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const cors = require('cors')
+app.use(cors()) /* To avoid cross origin error */
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+const URL =
+  'mongodb+srv://deployment:deployment123@cluster0.g23kn7u.mongodb.net/?retryWrites=true&w=majority'
+
+const mongoose = require('mongoose')
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+const connection = mongoose.connection
+connection.on('open', () => console.log('MongoDB Connected'))
+
+app.get('/', (req, res) =>
+  res.send('Welcome to sample API deployment for task practice.'),
+)
+
+app.get('/data', (req, res) =>
+  res.send([
+    { name: 'Ram', email: 'Ram@gmail.com' },
+    { name: 'Bob', email: 'bob32@gmail.com' },
+  ]),
+)
+
+app.listen(PORT, () => console.log(`Server started in the port ${PORT}`))
